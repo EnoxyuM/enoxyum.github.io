10023af35e1819d5fa1f64bdf0fbfc8cf5f35866
EnoxyuM <41592151+EnoxyuM@users.noreply.github.com>
2025-08-01 02:18:19 +0400

    Update gdrive.js

--- js/gdrive.js
+++ js/gdrive.js
@@ -1,7 +1,3 @@
-const API_KEY = 'AIzaSyD8KOJsO9BZoEFSBnzNgbHS_xN4cV9nrFQ';
-const CLIENT_ID = '342120408593-sm46uvqfjqrsigrkfnvnddpge9c9p9bh.apps.googleusercontent.com';
-
-
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest", "https://www.googleapis.com/discovery/v1/apis/picker/v1/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 
