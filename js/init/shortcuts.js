d28dd67ad417f0015cd6716e6f9a948669bf9b3c
EnoxyuM <41592151+EnoxyuM@users.noreply.github.com>
2026-01-20 22:38:57 +0400

    Update shortcuts.js

--- js/init/shortcuts.js
+++ js/init/shortcuts.js
@@ -1,11 +1,18 @@
 function setupShortcuts() {
     document.addEventListener('keydown', e => { 
         if (e.key === 'Escape') { 
-            toggleMenu(); 
+            if (isLauncherMode) {
+                e.preventDefault();
+                toggleLauncher(); // Toggle between scene and launcher
+            } else {
+                toggleMenu(); 
+            }
         } else if (e.ctrlKey && e.altKey && !e.shiftKey && !altPressed) { 
-            e.preventDefault(); 
-            altPressed = true; 
-            toggleEditor(); 
+            if (!isLauncherMode) {
+                e.preventDefault(); 
+                altPressed = true; 
+                toggleEditor();
+            }
         } else if (e.altKey && e.shiftKey && !shiftAltPressed) { 
             e.preventDefault(); 
             shiftAltPressed = true; 
@@ -25,10 +32,16 @@ function setupShortcuts() {
             scene.contentWindow.addEventListener('keydown', e => { 
                 if (e.key === 'Escape') { 
                     e.preventDefault(); 
-                    toggleMenu(); 
+                    if (isLauncherMode) {
+                        toggleLauncher();
+                    } else {
+                        toggleMenu(); 
+                    }
                 } else if (e.ctrlKey && e.altKey && !e.shiftKey) { 
-                    e.preventDefault(); 
-                    toggleEditor(); 
+                    if (!isLauncherMode) {
+                        e.preventDefault(); 
+                        toggleEditor(); 
+                    }
                 } else if (e.altKey && e.shiftKey) { 
                     e.preventDefault(); 
                     toggleConsole(); 
@@ -36,4 +49,4 @@ function setupShortcuts() {
             });
         }
     });
-} 
+}
